089b46f55d507b6efb10a5ed3fa29c76af3b6697
Danilo Barberini <danilo.barberini@gmail.com>
2021-04-04 23:43:36 -0300

    Changes in scripts

--- frontend/scripts/cadastrar-escola.js
+++ frontend/scripts/cadastrar-escola.js
@@ -1,15 +1,26 @@
 
 
-const school_name = document.getElementById("school_name")
-const schoolAddress = document.getElementById("schoolAddress")
+const CNPJ = document.getElementById("CNPJ");
+const razao = document.getElementById("razao");
+const fantasia = document.getElementById("fantasia");
+const address = document.getElementById("address");
+const kit = document.getElementById("kit");
+const qt = document.getElementById("qt");
+const phone = document.getElementById("phone");
+
 
 
 document.getElementById("enviar").addEventListener("click", function (event) {
     event.preventDefault();
 
     axios.post("http://localhost:3333/schools", {
-        "address": schoolAddress.value,
-        "name": school_name.value
+        "CNPJ": CNPJ.value,
+        "razao": razao.value,
+        "fantasia": fantasia.value,
+        "address": address.value,
+        "kit": kit.value,
+        "qt": qt.value,
+        "phone": phone.value
     })
         .then(() =>
             alert("Deu bom, meu brother."))
